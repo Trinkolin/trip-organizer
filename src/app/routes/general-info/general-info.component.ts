458af79671ce069751f3dfe625dcd83afb3397bc
trinkolin <trinkolin@gmail.com>
2024-06-26 23:50:15 +0200

    refactoring

--- src/app/routes/general-info/general-info.component.ts
+++ src/app/routes/general-info/general-info.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from "@angular/material/card";
 
 @Component({
   selector: 'app-general-info',
   standalone: true,
-  imports: [MatListModule],
+  imports: [MatListModule, MatCardModule],
   templateUrl: './general-info.component.html',
 })
 export class GeneralInfoComponent {
